3bfb691ae9ede4b00c9b3b69a6ba0981be5f2e44
kaybekov <kayumbekow@gmail.com>
2022-12-20 20:55:30 +0100

    Newly Organized Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Expenses from './components/Expenses'
-function App() {
+import React from 'react';
+import Expenses from './components/Expenses/Expenses'
+const App = () => {
   const expenses = [
     {
       id: 'e1',
