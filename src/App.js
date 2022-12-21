5604d84b34a748656a762d4a2be53aae75763b56
kaybekov <kayumbekow@gmail.com>
2022-12-21 12:05:03 +0100

    ExpenseForm add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses'
 const App = () => {
   const expenses = [
@@ -23,11 +25,21 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense)
+  };
 
+// return React.createElement(
+//   'div',
+//   {},
+//   React.createElement('h2',{},"Let's get started!"),
+//   React.createElement(Expenses,{items: expense}),
+// )
   
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
